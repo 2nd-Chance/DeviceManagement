a01f6da074c377667af7a18bff78b53b3feac465
Chang-Sub Kim <peurocs4@gmail.com>
2019-10-30 11:27:21 +0900

    Included a Map for the Room

---
+++ DeviceManagement/RoomMap.h
@@ -0,0 +1,19 @@
+#ifndef RoomMap_h__
+#define RoomMap_h__
+
+
+#include <cinttypes>
+
+#include "Room.h"
+
+namespace csk
+{
+	class RoomMap : public MyMap<std::uint32_t, std::shared_ptr<csk::Room>>
+	{
+	public:
+
+	};
+}
+
+
+#endif // RoomMap_h__
